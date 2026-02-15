1f8931da6dfc1ec7322e2601c8defbaee771d2ed
roaa <mroaa023@gmail.com>
2026-02-15 23:19:47 +0200

    Revert "Optimize bundle with code splitting and async image decoding"
    This reverts commit 03e8fc19b0ab11b8206693a220705f5deaf85040.

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: 'esbuild',
-    target: 'es2015',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          icons: ['react-icons']
-        }
-      }
-    },
-    cssCodeSplit: true,
-    assetsInlineLimit: 4096,
-    chunkSizeWarningLimit: 1000
-  }
 })
